6c71a544e17a04b0bd72e75f0ee6cab9460c8b37
heye <326524197@qq.com>
2019-10-18 12:55:47 +0800

    fix

--- consumer/src/main/java/com/springcloud/consumer/interf/FeignInterface.java
+++ consumer/src/main/java/com/springcloud/consumer/interf/FeignInterface.java
@@ -1,11 +1,12 @@
 package com.springcloud.consumer.interf;
 
+import com.springcloud.consumer.config.ServiceFeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="provider")
+@FeignClient(name="provider",configuration = ServiceFeignConfiguration.class)
 public interface FeignInterface {
 	@RequestMapping(value="/api/v1/",method = RequestMethod.GET)
 	public String home1(@RequestParam(value = "name") String name);
