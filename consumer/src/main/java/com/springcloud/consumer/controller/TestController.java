9a8dca20749cdae9343a28060740e38a09a6acd3
heye <326524197@qq.com>
2019-10-18 15:25:01 +0800

    fix

--- consumer/src/main/java/com/springcloud/consumer/controller/TestController.java
+++ consumer/src/main/java/com/springcloud/consumer/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.springcloud.consumer.controller;
 
+import com.springcloud.consumer.interf.FooService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,9 @@ public class TestController {
 	
 	@Autowired
 	FeignInterface feignInterface;
+
+	@Autowired
+	FooService fooService;
 	
 	@GetMapping("/getFeignInterface/{name}")
 	public String feign(@PathVariable("name") String name) {
@@ -23,4 +27,11 @@ public class TestController {
 	public String hello() {
 		return "hello my name is consumer";
 	}
+
+	@GetMapping("/api/v2")
+	public String hello22() {
+		fooService.get();
+		return "hello my name is consumer";
+	}
+
 }
